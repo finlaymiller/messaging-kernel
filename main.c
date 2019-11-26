ac9c4efc1850bb786507460a79d256dd2c599c4b
DESKTOP-JERB3J4\Derek <derekcapone7@gmail.com>
2019-11-26 17:03:13 -0400

    Quick bugfix on inserting processes into pri queue

--- main.c
+++ main.c
@@ -28,8 +28,11 @@ void main (void)
 
     // init procs here
     reg_proc(&procSendRecv, 1, 3);
+	//reg_proc(&procC, 1, 3);
+	//reg_proc(&procA, 1, 3);
+    //reg_proc(&procB, 1, 3);
 
-    reg_proc(&procSend, 2, 3);
+    //reg_proc(&procSend, 2, 3);
 
     initRunning();
 
